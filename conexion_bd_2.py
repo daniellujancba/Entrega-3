91e8f2f3153573626ad12d9d4b07d8f30e2b10ee
Daniel Lujan <daniel.lujan.cba@gmail.com>
2023-06-11 10:24:37 -0300

    aqui estableci la conexion correcta, hice main, menu y funcion para ver bases de datos

---
+++ conexion_bd_2.py
@@ -0,0 +1,65 @@
+import mysql.connector
+
+
+class Conectar():
+
+    def __init__(self) -> None:
+        try:
+            self.conexion = mysql.connector.connect(
+                host = 'localhost',
+                port = 3306,
+                user = input("Ingrese el Usuario: "), # root
+                password = input("Ingrese el Password: "),# " " - vacio + enter
+                db= input("Ingrese el nombre de la Base de Datos: ")
+            )
+            if self.conexion.is_connected():
+                print("")
+                print("LA CONEXION ES EXITOSA")
+                print("")
+                
+        except:
+            print("NO SE PUDO CONETAR A LA BASE DE DATOS")
+
+
+    def traer_db(self):   # MUESTRA EL LISTADO DE BASES EN MYSQL     
+        try:
+            if self.conexion.is_connected():
+                print("esta conectado")       
+                mi_cursor = self.conexion.cursor()
+                print("Para mostrar el listado de bases de datos")
+                query = str(input("Ingrese la sentencia en mayuscula:")) # SHOW DATABASES
+                mi_cursor.execute(query)
+                for db in mi_cursor:
+                    print (db)
+            
+                mi_cursor.close()
+                self.conexion.close()          
+        except:
+            print("NO SE PUDO CONETAR A LA BASE DE DATOS")     
+
+
+##############################################################################################
+"""
+print("")
+print("")
+print("***********************************************************")
+print("******* Bienvenidos a la Base de Datos de Big Bread *******")
+print("***********************************************************")
+print("")
+print("Para acceder debera ingresar su Usuario y Password y elegir una Base de Datos")
+tor = Conectar()
+print("Desea realizar una consulta?")
+eleccion = str(input("Ingrese -y- en caso afirmativo o -n- en caso negativo: "))
+if eleccion == "y":
+    print("")
+    print("Seleccione una opcion: ")
+    print("1 - Ver bases de datos")
+    print("2 - Crear una BD")
+    print("3 - Eliminar un BD")
+    print("4 - Ver tablas en BD")
+    print("5 - Crear tabla en BD")
+    print("6 - Eliminar tabla en BD")
+    print("7 - Insertar insumo en Tabla")
+    print("8 - Listado de insumos")
+
+    """
---
+++ main.py
@@ -0,0 +1,27 @@
+from conexion_bd_2 import Conectar
+
+print("")
+print("")
+print("***********************************************************")
+print("******* Bienvenidos a la Base de Datos de Big Bread *******")
+print("***********************************************************")
+print("")
+print("Para acceder debera ingresar su Usuario y Password y elegir una Base de Datos")
+tor = Conectar()
+print("Desea realizar una consulta?")
+eleccion = str(input("Ingrese -y- en caso afirmativo o -n- en caso negativo: "))
+if eleccion == "y":
+    print("")
+    print("Seleccione una opcion: ")
+    print("1 - Ver bases de datos")
+    print("2 - Crear una BD")
+    print("3 - Eliminar un BD")
+    print("4 - Ver tablas en BD")
+    print("5 - Crear tabla en BD")
+    print("6 - Eliminar tabla en BD")
+    print("7 - Insertar insumo en Tabla")
+    print("8 - Listado de insumos")
+
+    opcion = str(input("Ingrese numero: "))
+    if opcion == "1":
+        tor.traer_db()
